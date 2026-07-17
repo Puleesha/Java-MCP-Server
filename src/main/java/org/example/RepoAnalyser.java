0ba8f670c97f48869406c0d55bb198061a3eb51d
Puleesha <puleeshak@gmail.com>
2026-07-17 12:27:15 +0530

    Generated javadocs

--- src/main/java/org/example/RepoAnalyser.java
+++ src/main/java/org/example/RepoAnalyser.java
@@ -13,7 +13,7 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- *  This class represent a repository analyser, where it can access files within a folder and return the TODOs present.
+ * This class represents a repository analyser, where it can access files within a folder and return the TODOs present.
  *
  * @author  Puleesha Vilhan
  * @since   05/12/2025
@@ -21,17 +21,30 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class RepoAnalyser {
     private static final Logger log = LoggerFactory.getLogger(RepoAnalyser.class);
 
+    // Maximum time a request can take
     private static final Duration REQUEST_DEADLINE = Duration.ofSeconds(5);
+    // Maximum length of the response
     private static final int REQUEST_LENGTH_LIMIT = 2000;
 
+    // Counter for the total number of TODOs found
     private final AtomicInteger todoCount = new AtomicInteger(0);
+    // Counter for the total number of files analyzed
     private final AtomicInteger fileCount = new AtomicInteger(0);
+    // Semaphore to limit the number of concurrent file analysis tasks
     private final Semaphore connections = new Semaphore(100);
 
+    // List to store the TODOs found
     private final ArrayList<String> TODOs;
+    // Deadline for the request
     long deadline;
+    // Task limit for the request
     int taskLimit;
 
+    /**
+     * Constructor for the RepoAnalyser class.
+     *
+     * @param limit The maximum number of tasks that can be processed
+     */
     public RepoAnalyser(int limit) {
         deadline = System.nanoTime() + REQUEST_DEADLINE.toNanos();
         taskLimit = limit;
@@ -39,11 +52,10 @@ public class RepoAnalyser {
     }
 
     /**
-     * List all the files that have to be analysed
+     * Analyzes the repository located at the specified folder path.
      *
-     * @param   folderPath The folder path of the repository
-     *
-     * @return  A List of the paths of all files
+     * @param folderPath The path to the folder containing the repository
+     * @return A list of file paths to be analyzed
      */
     public List<Path> analyzeRepository(String folderPath) {
         Path rootDir = Paths.get(folderPath);
@@ -63,6 +75,13 @@ public class RepoAnalyser {
         return filesToAnalyze;
     }
 
+    /**
+     * Discovers all regular files within the specified directory.
+     *
+     * @param rootDir The root directory to start the search
+     * @return A list of file paths
+     * @throws IOException If an I/O error occurs
+     */
     private List<Path> discoverFiles(Path rootDir) throws IOException {
         List<Path> result = new LinkedList<>();
 
@@ -76,12 +95,11 @@ public class RepoAnalyser {
     }
 
     /**
-     * This method analyses the file and will be called sequentially or concurrently based on the server variant
+     * Analyzes the specified file for TODO comments.
      *
-     * @param   file The path of the file
-     *
-     * @throws  IOException If the reader throws and error
-     * @throws  InterruptedException If the Thread.sleep() is interrupted
+     * @param file The path of the file to analyze
+     * @throws IOException If the reader throws an error
+     * @throws InterruptedException If the Thread.sleep() is interrupted
      */
     public void analyzeFile(Path file) throws IOException, InterruptedException {
         try {
@@ -105,9 +123,9 @@ public class RepoAnalyser {
     }
 
     /**
-     * Add a task to the array list
+     * Adds a TODO comment to the list if the limits are not reached.
      *
-     * @param   line The comment to be added
+     * @param line The TODO comment to add
      */
     private synchronized void addTODO(String line) {
         String newTask = line.replace("//", " ");
@@ -121,9 +139,9 @@ public class RepoAnalyser {
     }
 
     /**
-     * Get the total length of all tasks currently in the array list
+     * Calculates the total length of all TODO comments in the list.
      *
-     * @return  length of all array list tasks
+     * @return The total length of all TODO comments
      */
     private synchronized int getResponseLength() {
         int totalLength = 0;
@@ -134,22 +152,37 @@ public class RepoAnalyser {
         return totalLength;
     }
 
+    /**
+     * Retrieves the total number of files analyzed.
+     *
+     * @return The number of files analyzed
+     */
     public int getFileCount() {
         return fileCount.get();
     }
 
+    /**
+     * Retrieves the total number of TODOs found.
+     *
+     * @return The number of TODOs found
+     */
     public int getTodoCount() {
         return todoCount.get();
     }
 
+    /**
+     * Retrieves the list of TODOs found.
+     *
+     * @return A string representation of the list of TODOs
+     */
     public synchronized String getTODOs() {
         return TODOs.toString();
     }
 
     /**
-     * Checks if the response length or the task limit or time limit is reached
+     * Checks if any of the limits (task limit, response length, time limit) have been reached.
      *
-     * @return  Boolean indicating if any of the limits were reached
+     * @return Boolean indicating if any limits have been reached
      */
     public boolean isLimitReached() {
         return (todoCount.get() >= taskLimit) || (System.nanoTime() > deadline);
